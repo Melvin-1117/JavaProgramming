083710c74fec2ff5010e24aa3e0761a793c92ef3
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-09 13:44:28 +0530

    Maximum number in a array

--- Arrays/MaxInArray.java
+++ Arrays/MaxInArray.java
@@ -8,11 +8,11 @@ public class MaxInArray {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        int min = arr[0];
+        int max= arr[0];
         for (int i =1 ; i< arr.length; i++){
-            if(arr[i]<min){
-                min= arr[i];
+            if(arr[i]>max){
+                max= arr[i];
             }
-        }System.out.println(min);
+        }System.out.println("Maximum element in the array is: " + max);
     }
 }
