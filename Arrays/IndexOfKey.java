f58a5f3946c76ebe54f7cdb7b698e46a3f740135
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-10 15:06:11 +0530

    Return the index of the key

--- Arrays/IndexOfKey.java
+++ Arrays/IndexOfKey.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class IndexOfKey {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the array size:");
         int n = sc.nextInt();
         int arr[] = new int[n];
         int index =0;
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        System.out.print("Enter the key: ");
         int k=sc.nextInt();
         for(int i=0;i<n;i++){
            if(arr[i]==k){
@@ -15,8 +17,9 @@ public class IndexOfKey {
             break;
            }
            
-        } System.out.println(index);
-
+        } 
+        System.out.println("Index of the key is "+index);
+        sc.close();
     }
     
 }
