55d99ae58ebcd97fb73faaf5b103ba7d8ab8e32e
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-10 22:54:47 +0530

    Count the frequency of the array

--- Arrays/Frequency.java
+++ Arrays/Frequency.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Frequency {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the element:"+" ");
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int count=0;
+            int count1=0;
+            for(int j=i+1;j<n;j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if(count==0) {
+                for (int k = 0; k < i + 1; k++) {
+                    if (arr[k] == arr[i]) {
+                        count1++;
+                    }
+                }
+                System.out.println(arr[i]+"-"+count1);
+            }
+        }
+    }
 }
