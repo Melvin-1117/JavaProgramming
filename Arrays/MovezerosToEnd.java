cde4c59390a4a34fbbd2f852bdc03d1649bd0a2c
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-11 23:13:58 +0530

    Move all the zeroes in the array to the end

--- Arrays/MovezerosToEnd.java
+++ Arrays/MovezerosToEnd.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class MovezerosToEnd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int res[] = new int[n];
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                res[index]=arr[i];
+                index++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+
 }
