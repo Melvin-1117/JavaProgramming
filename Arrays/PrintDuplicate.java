6ce9e1fc31bbc31753064bbcb5d9e577ec1beb31
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-11 11:50:44 +0530

    Print the number which have a duplicate number in it

--- Arrays/PrintDuplicate.java
+++ Arrays/PrintDuplicate.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class PrintDuplicate {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+            for(int i=0;i<n;i++){
+                int flag=0;
+                for(int j=0;j<n;j++) {
+                    if (arr[i]==arr[j]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag==1){
+                    for(int k=i+1;k<n;k++){
+                        if(arr[i]==arr[k]){
+                            System.out.print(arr[i]+" ");
+                        }
+                    }
+                }
+
+        }
+    }
 }
