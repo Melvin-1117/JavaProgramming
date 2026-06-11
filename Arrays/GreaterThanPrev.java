7830fc141e88dec00cde88afd6b785d0ffcfdbe3
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-11 13:18:11 +0530

    Print the element if it is greater than previous

---
+++ Arrays/GreaterThanPrev.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class GreaterThanPrev {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int [n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int prev=arr[0];
+        System.out.print(prev+" ");
+        for(int i=1;i<n;i++){
+            if(arr[i]>prev){
+                prev=arr[i];
+                System.out.print(prev+" ");
+            }
+        }
+    }
+}
