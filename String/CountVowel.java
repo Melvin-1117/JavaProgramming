8185fa427de3b140bef4c3da2612a2cc137c973b
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-12 22:09:41 +0530

    Count the number of vowels in the string

---
+++ String/CountVowel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CountVowel{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String input= sc.nextLine();
+        int vowel=0;
+        int consonant=0;
+        for(int i=0;i<input.length();i++){
+            char ch = input.charAt(i);
+            if((ch>='A'&&ch>='Z')||(ch=='a'&&ch=='z')){
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    vowel++;
+                }else{
+                    consonant++;
+                }
+            }
+        }
+        System.out.println("vowels "+vowel);
+        System.out.println("Consonants "+consonant);
+    }
+}
