672904f92ce2a30af5c7f229b57f36a3544de98e
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-12 13:41:01 +0530

    Print the entered string in the output

--- Arrays/RemoveDuplicate.java
+++ Arrays/RemoveDuplicate.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class RemoveDuplicate {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int count=0;
+         for(int j=0;j<n;j++){
+             if(arr[i]==arr[j]){
+                 count++;
+             }
+         }
+         if(count==1) {
+             System.out.print(arr[i] + " ");
+         }
+        }
+    }
 }
