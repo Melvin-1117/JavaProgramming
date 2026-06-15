293fc7da7b844d9d7d3b1d806830e42b2d5bd748
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-15 10:59:17 +0530

    Check whether the given two string is anagram or not

---
+++ String/Anagram.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Anagram {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        if(str1.length()!=str2.length()){
+            System.out.print("Not anagram");
+        }else {
+            int count = 0;
+            for (int i = 0; i < str1.length(); i++) {
+                char ch1 = str1.charAt(i);
+                for (int j = 0; j < str2.length(); j++) {
+                    if (ch1 == str2.charAt(j)) {
+                        count++;
+                    }
+                }
+            }
+            if (count == str1.length()) {
+                System.out.print("Anagram");
+            }
+        }
+    }
+}
