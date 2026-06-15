87a026dac717345edfe93386d78d07dbb08d092f
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-15 11:40:19 +0530

    Check the anagram even with symbols and numbers

--- String/Anagram.java
+++ String/Anagram.java
@@ -6,20 +6,25 @@ public class Anagram {
         String str2 = sc.nextLine();
         if(str1.length()!=str2.length()){
             System.out.print("Not anagram");
-        }else {
-            int count = 0;
+        }
+        else {
+            int arr[] = new int[254];
             for (int i = 0; i < str1.length(); i++) {
-                char ch1 = str1.charAt(i);
-                for (int j = 0; j < str2.length(); j++) {
-                    if (ch1 == str2.charAt(j)) {
-                        count++;
-                    }
+                arr[str1.charAt(i)]++;
+            }
+            for (int j = 0; j < str2.length(); j++) {
+                arr[(str2.charAt(j))]--;
+            }
+            int flag = 0;
+            for (int k = 0; k < 254; k++) {
+                if (arr[k] != 0) {
+                    flag = 1;
                 }
             }
-            if (count == str1.length()) {
-                System.out.print("Anagram");
-            } else{
-                System.out.print("Not a anagram");
+            if (flag == 1) {
+                System.out.println("Not an anagram");
+            } else {
+                System.out.println("Anagram");
             }
         }
     }
