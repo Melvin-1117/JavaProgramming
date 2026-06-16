18eda39406ffefd06f67ec714e30536592fe2af4
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-16 22:44:47 +0530

    Second largest element in the array

--- Arrays/SecondMinimum.java
+++ Arrays/SecondMinimum.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class SecondMinimum {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int min=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]<min){
+                arr[i]=min;
+            }
+        }
+        int smin=arr[3];
+        for(int i=1;i<n;i++){
+            if(arr[i]<smin && arr[i]!=min){
+                smin=arr[i];
+            }
+        }
+        System.out.print(smin);
+    }
 }
