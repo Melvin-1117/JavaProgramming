52f450032fd423faa7bdbd5067de3b44d69fa567
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-17 11:57:33 +0530

    Find the maximum element in the column

--- Matrices/MaxInCol.java
+++ Matrices/MaxInCol.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
-public class MaxInCol {
+import java.util.Scanner;
+public class MaxIncol {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=n;
+        int c=n;
+        int arr[][]=new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < c; i++) {
+            int max=arr[0][0];
+            for (int j = 0; j < r; j++) {
+                if (arr[j][i] > max) {
+                    max = arr[j][i];
+                }
+            }
+            System.out.print(max+" ");
+        }
+    }
 }
