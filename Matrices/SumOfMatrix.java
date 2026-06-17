53ee2f5ec5019a5fa78646b3863adb607115ad7e
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-17 13:54:13 +0530

    Sum of allelements in a matrix

--- Matrices/SumOfMatrix.java
+++ Matrices/SumOfMatrix.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
-public class SumOfTwoMatrix {
-    Scanner sc = new Scanner(System.in);
-    int
-}
+public class SumOfMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int r = n;
+        int c = n;
+        int arr[][] = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        int sum=0;
+        for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum = sum+arr[i][j];
+        }
+    }
+        System.out.print(sum);
+     }
+    }
