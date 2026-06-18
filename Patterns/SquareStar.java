30b1fd0dba36b0666e71657cce249266ec47656a
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-18 21:29:44 +0530

    Pattern 1

--- Patterns/SquareStar.java
+++ Patterns/SquareStar.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class SquareStar {
+        public static void main(String [] args){
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    System.out.print("*");
+                }System.out.println();
+            }
+    }
 }
+
+
+
+/*
+ *****
+ *****
+ *****
+ *****
+ *****
+*/
