e12047a8a49d6333c7f05058a5abf331c7a00f2d
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-18 21:17:29 +0530

    Patter 3

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Pyramid {
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=n-i;j>0;j--){
+                System.out.print(" ");
+            }for(int k=0;k<2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
+
+/*
+
+    *
+   ***
+  *****
+ *******
+
+ */
