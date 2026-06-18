1f2292e6bba4e38da55c4dd8951cb1f202cbdef8
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-18 21:23:07 +0530

    Pattern 3

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -3,7 +3,7 @@ public class Pyramid {
     public static void main(String [] args){
         Scanner sc = new Scanner (System.in);
         int n=sc.nextInt();
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=n-i;j>0;j--){
                 System.out.print(" ");
             }for(int k=0;k<2*i-1;k++){
