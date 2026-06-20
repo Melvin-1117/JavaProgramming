7676d348bb32f41f057a8c5c5c2481421b34a28e
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-20 19:53:34 +0530

    Sort the elements using selection sort

---
+++ Arrays/LeftRotate.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LeftRotate {
+    public static void main[String () args]{
+            Scanner sc = new Scanner(System.in);
+            int n=sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+        }int k=sc.nextInt();
+                k=k%n;
+            int temp[]=new int[k];
+            for(int i=0;i<k;i++){
+                temp[i]=arr[i];
+            }
+            System.out.print(temp);
+    }
+}
