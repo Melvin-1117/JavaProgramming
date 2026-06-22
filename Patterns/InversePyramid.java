807a0ee02efb6bb74da4899868e137a1d9a98602
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-22 19:35:09 +0530

    Pattern 8

--- Patterns/InversePyramid.java
+++ Patterns/InversePyramid.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class InversePyramid {
-}
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*(n-i)+1;k++) {
+                System.out.print("*");
+            }
+                System.out.println();
+            }
+        }
+    }
+
