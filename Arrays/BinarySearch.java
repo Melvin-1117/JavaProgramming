5c85047245ce3234cd0592453abaf1c5947da56b
Melvimn-1117 <antonymelvin81@gmail.com>
2026-06-24 09:05:32 +0530

    Search element using binary search algorithm

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class BinarySearch {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int low=0;
+        int high = n-1;
+        int target=sc.nextInt();
+        while(low<=high) {
+            int mid =(low+high)/2;
+            if (arr[mid]==target) {
+                 System.out.print(mid);
+                 break;
+            } else if (arr[mid] > target) {
+                high= mid-1;
+            } else {
+                low=mid+1;
+            }
+        }
+    }
 }
